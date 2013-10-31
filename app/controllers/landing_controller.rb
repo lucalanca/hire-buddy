f9c6c412efed9a680035b4b55796c189856c0aa8
Joao Figueiredo <lucalanca@hotmail.com>
2013-10-31 21:46:05 +0000

    Initial commit.

---
+++ app/controllers/landing_controller.rb
@@ -0,0 +1,7 @@
+class LandingController < ApplicationController
+  def welcome
+  end
+
+  def styleguide
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HireBuddy::Application.config.secret_key_base = 'c621dfb5c523c6e5ac464250600eb4b339ed545ffbed55704a8b1ebe9de597c9c448ce52ae1d392cf885c415930d849a4dd90b583b6377b939fee0055151b1a7'
